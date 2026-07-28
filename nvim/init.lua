cb2d033d621311fb1a5b636d018b49aefdd23f11
Robert Hardy <robbert@gmail.com>
2026-07-28 06:18:10 +0100

    Exclude the saved_pdfs directory from file finder.

--- nvim/init.lua
+++ nvim/init.lua
@@ -372,11 +372,9 @@ require("lazy").setup({
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
+				defaults = {
+					file_ignore_patterns = { "^saved_pdfs/" },
+				},
 				-- pickers = {}
 				extensions = {
 					["ui-select"] = {
